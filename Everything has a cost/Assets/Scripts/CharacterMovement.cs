321615c8ac013628f5f112367a22a5fab0bb2e51
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2018-06-25 22:37:05 +0200

    Working on feather controller

--- Everything has a cost/Assets/Scripts/CharacterMovement.cs
+++ Everything has a cost/Assets/Scripts/CharacterMovement.cs
@@ -9,54 +9,67 @@ public class CharacterMovement : MonoBehaviour
     public float jumpVelocity = 10;
     private Rigidbody2D rb;
     public LayerMask Jumpable;
-    //Animation & feathers
-    private bool movingLeft = false;
-    private bool movingRight = false;
-    private bool jumping = false;
     private float groundedDist = 1;
+    private Animator anim;
+    bool grounded = true;
 
-	void Start ()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-	}
+        anim = GetComponent<Animator>();
+    }
 
-	void FixedUpdate ()
+    void FixedUpdate()
     {
         Move();
         Jump();
+
+        CheckGrounded();
     }
 
     void Move()
     {
         rb.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+        
+        //Flip sprite and control animations
+        if (rb.velocity.x != 0)
+        {
+            anim.SetBool("IsMoving", true);
+
+            if (rb.velocity.x > 0)
+            {
+                if (transform.localScale.x != 1) { transform.localScale = new Vector3(1, 1, 1); }
+            }
+            else if (rb.velocity.x < 0)
+            {
+                if (transform.localScale.x != -1) { transform.localScale = new Vector3(-1, 1, 1); }
+            }
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
     }
 
     void Jump()
     {
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity +=  new Vector2 (0, jumpVelocity);
+            rb.velocity += new Vector2(0, jumpVelocity);
+            anim.SetTrigger("Wingbeat");
         }
     }
 
-    bool IsGrounded()
+    void CheckGrounded()
     {
-        Debug.DrawLine(
-            transform.position,
-            transform.position - new Vector3 (0, - groundedDist, 0),
-            Color.red,
-            0.5f);
-
         Vector2 transform2D = Transform2D();
-        return Physics2D.Linecast(
-            transform2D,
-            transform2D + new Vector2 (0, -groundedDist),
-            Jumpable);
+        anim.SetBool("IsGrounded", Physics2D.Linecast(transform2D, transform2D + new Vector2(0, -groundedDist), Jumpable));
+
+        //Debug.DrawLine(transform.position, transform.position + new Vector3(0, -groundedDist, 0), Color.red, 0.5f);
     }
 
     Vector2 Transform2D()
     {
         return new Vector2(transform.position.x, transform.position.y);
     }
-
 }
---
+++ Everything has a cost/Assets/Scripts/FeatherController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatherController : MonoBehaviour {
+
+    enum FeatherState
+    {
+        empty,
+        travelling,
+        ocuppied
+    };
+
+    public Transform LWing;
+    public GameObject[] feathers;
+
+    private FeatherState[] feathersState;
+    private int featherRows = 10;
+    private int[] feathersInEachRow;
+    private float radius = 10;//radius changes =/
+    private float maxAngle;//A fixed angle value (where the body of the bird starts)
+
+    //Control both wings on the same script
+
+
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+        int featherNum = 0;
+        float currentAngle = LWing.transform.rotation.z;
+
+        for (int i = 0; i < featherRows; ++i)
+        {
+            float featherSeparation = (maxAngle - currentAngle) / feathersInEachRow[i];
+
+            for (int j = 0; j < feathersInEachRow[i]; ++j)
+            {
+                feathers[featherNum].transform.position = new Vector3(
+                        radius * Mathf.Cos(currentAngle + featherSeparation * j),
+                        radius * Mathf.Sin(currentAngle + featherSeparation * j),
+                        feathers[featherNum].transform.position.z);
+                ++featherNum;
+            }
+        }
+
+        //foreach (GameObject f in feathers)
+        //{
+
+        //}
+	}
+}
